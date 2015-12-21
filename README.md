cebf40b4d6f3c0587fac205393ac0d739539c51e
Erik Dasque <edasque@constantcontact.com>
2015-12-21 12:46:36 -0500

    Added thats-what-time-it-is container & ECS json files

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ ECS Sample App
 =====
 
 A basis for a Elastic Container Service tutorial
+
+* A container that holds a HTTP server that listens on  port 8000 (```curl http://hostname:8000```) and TCP server that listens on port 9000 (```nc hostname 9000```)
+* a process that generates random files names which content is the current datetime in a given volume
+* a container that syncs that volume to an S3 bucket
+
+Generating ECS JSON files
+---
+Those were converted from docker-compose files using [container-tranform](https://github.com/ambitioninc/container-transform) (```pip install container-transform```).
+
+TODO
+---
+* ~~Figure out why I can reach the HTTP server (port 8000) but not the TCP one (port 9000) ~~ - it was because it was listening on *localhost* instead of *0.0.0.0*
+* Also, I don't think the cron back-up works on ECS
---
+++ thats-what-time-it-is/thats-what-time-it-is.js
@@ -0,0 +1,37 @@
+// Load the http module to create an http server.
+var http = require('http');
+
+// Load the net module to create a tcp server.
+var net = require('net');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+
+    var now = new Date();
+
+    var time = now.getHours() + ':' + ((now.getMinutes() < 10) ? ("0" + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ("0" + now.getSeconds()) : (now.getSeconds()));
+    response.end(time+", that's what time it is.");
+
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
+
+// Creates a new TCP server. The handler argument is automatically set as a listener for the 'connection' event
+var tcp_server = net.createServer(function (socket) {
+
+  // Every time someone connects, tell them hello and then close the connection.
+  console.log("Connection from " + socket.remoteAddress);
+  socket.end("Hello World from TCP\n");
+
+});
+
+// Fire up the server bound to port 7000 on localhost
+tcp_server.listen(9000, "0.0.0.0");
+
+// Put a friendly message on the terminal
+console.log("TCP server listening on port 9000 at localhost. Communicate with 'nc localhost 7000'");
